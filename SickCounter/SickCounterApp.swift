0a6b0a8ea353d6b76cdf0da3560ca4d0328db594
Soorria Saruva <soorria.ss@gmail.com>
2023-12-16 14:42:09 +1100

    Initial Commit

---
+++ SickCounter/SickCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SickCounterApp.swift
+//  SickCounter
+//
+//  Created by mooth on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SickCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
